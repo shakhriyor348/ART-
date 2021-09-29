264ad3329deeb0ca3f95194cab741a759db427f9
shakhriyor348 <62033235+shakhriyor348@users.noreply.github.com>
2021-09-29 22:55:04 +0300

    finallyCommit

---
+++ src/js/modules/drop.js
@@ -0,0 +1,88 @@
+
+import { postData } from "../services/requests";
+const drop = () => {
+    // drag *
+    // dragend *
+    // dragenter - объект над dropArea
+    // dragexit *
+    // dragleave - объект за пределами dropArea
+    // dragover - объект зависает над dropArea
+    // dragstart *
+    // drop - объект отправлен в dropArea
+    
+    const fileInputs = document.querySelectorAll('[name="upload"]');
+
+    ['dragenter', 'dragleave', 'dragover', 'drop'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, preventDefaults, false);
+        });
+    });
+
+    function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    function highlight(item) {
+        item.closest('.file_upload').style.border = "5px solid yellow";
+        item.closest('.file_upload').style.backgroundColor = "rgba(0,0,0, .7)";
+    }
+
+    function unhighlight(item) {
+        item.closest('.file_upload').style.border = "none";
+        if (item.closest('.calc_form')) {
+            item.closest('.file_upload').style.backgroundColor = "#fff";
+        } else {
+            item.closest('.file_upload').style.backgroundColor = "#ededed";
+        }
+    }
+
+    ['dragenter', 'dragover'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, () => highlight(input), false);
+        });
+    });
+
+    ['dragleave', 'drop'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, () => unhighlight(input), false);
+        });
+    });
+
+    const postFiles = async (url, data) => {
+        let res = await fetch(url, {
+            method: 'POST',
+            body: data
+        })
+    
+        return await res.text()
+    }
+
+    // const path = {
+    //     designer: 'assets/server.php',
+    //     question: 'assets/question.php'
+    // }
+
+    fileInputs.forEach(input => {
+        input.addEventListener('drop', (e) => {
+            input.files = e.dataTransfer.files;
+            let dots;
+            // console.log(input.files);
+            const arr = input.files[0].name.split('.');
+            // console.log(arr);
+            fetch('assets/server.php')
+                .then(res => {
+                    console.log(res)
+                    res.text().then(res2 => {
+                        console.log(res2);
+                    })
+                })
+            
+            arr[0].length > 6 ? dots = "..." : dots = '.';
+            const name = arr[0].substring(0, 6) + dots + arr[1];
+            input.previousElementSibling.textContent = name;
+        });
+    });
+};
+
+export default drop;
